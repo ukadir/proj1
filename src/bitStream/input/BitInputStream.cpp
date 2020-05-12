1a54ec0498bf54ca5dc2dbca2b9519064daab532
Sander Valstar <sander@email.com>
2020-05-12 12:57:32 -0700

    initial commit

---
+++ src/bitStream/input/BitInputStream.cpp
@@ -0,0 +1,38 @@
+#include "BitInputStream.hpp"
+
+/**
+ * TODO: Fill the buffer of size bufSize from the input stream.
+ */
+void BitInputStream::fill() {}
+
+/**
+ * TODO: Return true when the istream tried to read more bytes than the
+ * remaining bytes in the file and the buffer was also fully read. Otherwise,
+ * return false.
+ *
+ * Note that eof() is different from this function. eof() in most other istreams
+ * is only true when we are trying to read past the end of the file. Whereas
+ * this function should be only true when we are at the end of the file, but
+ * have not read past it yet. For example:
+ *
+ * // some code to read all the data in the file
+ * atEndOfFile(); // atEndOfFile() = true. But eof() is still false for now.
+ * readBit();     // trying to read past the end of the file
+ * eof();         // eof is true now
+ */
+bool BitInputStream::atEndOfFile() { return false; }
+
+/* Returns true when trying to read past the end of the file, false otherwise */
+bool BitInputStream::eof() { return eofBit; }
+
+/**
+ * TODO: Read the next bit from the bit buffer. Fills the buffer from the input
+ * stream if all the bits have already been read. It should return 0 if the bit
+ * read is 0, and return 1 if the bit read is 1.
+ *
+ * Before trying to read a bit from the buffer, if atEndOFfile() is true, set
+ * the eofBit and directly return 0 (You can return any number here, since after
+ * setting eofBit to true, eof() will return true and the program won’t be using
+ * the returned value from readBit).
+ */
+unsigned int BitInputStream::readBit() { return 0; }
---
+++ src/bitStream/input/BitInputStream.hpp
@@ -0,0 +1,34 @@
+#ifndef BITINPUTSTREAM_HPP
+#define BITINPUTSTREAM_HPP
+
+#include <iostream>
+
+typedef unsigned char byte;
+
+using namespace std;
+
+class BitInputStream {
+  private:
+    istream& in;           // reference to the input stream to use
+    char* buf;             // buffer of an char array
+    unsigned int bufSize;  // size of buffer in byte
+    unsigned int nbits;    // number of bits that have been read from buf
+    bool eofBit;           // true when trying to read past the end of the file
+
+    bool atEndOfFile();
+
+  public:
+    /**
+     * TODO: Initializes member variables. In particular, the char array should
+     * have size bufSize.
+     */
+    explicit BitInputStream(istream& is, unsigned int bufSize) : in(is){};
+
+    void fill();
+
+    bool eof();
+
+    unsigned int readBit();
+};
+
+#endif
